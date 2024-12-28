185fbedfa30ef41a07b2675fb2aeea1fbb2ab1c6
unzilashakir <unzilashakir21@gmail.com>
2024-12-28 17:11:48 +0500

    login form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loginform from './Components/Loginform/Loginform'
+
+const App = () => {
+  return (
+    <>
+    <Loginform />
+    </>
+  )
+}
+
+export default App
+
